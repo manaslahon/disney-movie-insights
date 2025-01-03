14b47c6e5c9993e0809c96d37106aa599b227948
manaslahon <manaslahonworking@gmail.com>
2025-01-03 14:15:53 +0530

    corrected path

--- scraper/utils/save_json.py
+++ scraper/utils/save_json.py
@@ -1,20 +1,26 @@
 import pickle
 import json
+import os
 
-
-def save_data(title, data):
-    with open(title, 'w', encoding='utf-8') as f:
+def save_data(title, data, directory='dataset'):
+    os.makedirs(directory, exist_ok=True)
+    file_path = os.path.join(directory, title)
+    with open(file_path, 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii= False, indent= 2)
  
  
-def load_data(title):
-    with open(title, encoding='utf-8') as f:
+def load_data(title, directory='dataset'):
+    file_path = os.path.join(directory, title)
+    with open(file_path, encoding='utf-8') as f:
         return json.load(f)
 
-def save_data_pickle(name, data):
-    with open(name, 'wb') as f:
+def save_data_pickle(title, data, directory='dataset'):
+    os.makedirs(directory, exist_ok=True)
+    file_path = os.path.join(directory, title)
+    with open(file_path, 'wb') as f:
         pickle.dump(data, f)
 
-def load_data_pickle(name):
-    with open(name, 'rb') as f:
+def load_data_pickle(title, directory='dataset'):
+    file_path = os.path.join(directory, title)
+    with open(file_path, 'rb') as f:
         return pickle.load(f)
